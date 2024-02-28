ef9d68e364d77b4fe4b56a3ba22d6027a0682b45
FeAlias <FeAlias@users.noreply.github.com>
2024-02-28 10:03:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ document.getElementById('startButton').addEventListener('click', function() {
 
     displayNextWord();
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, err => {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
